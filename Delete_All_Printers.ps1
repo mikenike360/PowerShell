d8a52d539750001b416db4388d642fd33dcd0e92
mikenike360 <michael.venema2010@gmail.com>
2020-04-13 14:23:49 -0700

    Create Delete_All_Printers.ps1

---
+++ Delete_All_Printers.ps1
@@ -0,0 +1,25 @@
+Function Delete-Printers
+{
+	$NetworkPrinters = Get-WmiObject -Class Win32_Printer
+	If ($NetworkPrinters -ne $null)
+	{
+		Try
+		{
+			Foreach($NetworkPrinter in $NetworkPrinters)
+			{
+				$NetworkPrinter.Delete()
+				Write-Host "Successfully deleted the network printer:" + $NetworkPrinter.Name -ForegroundColor Green	
+			}
+		}
+		Catch
+		{
+			Write-Host $_
+		}
+	}
+	Else
+	{
+		Write-Warning "Cannot find network printer in the currently environment."
+	}
+}
+
+Delete-Printers
