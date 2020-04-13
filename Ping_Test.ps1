ab265faf525bd1552151b5f57908958776cdc821
mikenike360 <michael.venema2010@gmail.com>
2020-04-13 14:40:52 -0700

    Update Ping_Test.ps1

--- Ping_Test.ps1
+++ Ping_Test.ps1
@@ -5,4 +5,4 @@ clear-host
 $computer = $env:COMPUTERNAME
 
 
-Ping.exe -t 172.16.64.10 | ForEach {"{0} - {1}" -f (Get-Date),$_} | Out-File (<SHARED LOCATION TO SAVE LOGS" + $computer + "_ping_log.txt")
+Ping.exe -t <IP or FQDN of host to ping> | ForEach {"{0} - {1}" -f (Get-Date),$_} | Out-File (<SHARED LOCATION TO SAVE LOGS" + $computer + "_ping_log.txt")
